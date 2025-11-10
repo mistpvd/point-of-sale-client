620a579f7562de2e5f2e9fa41afc48d2490b723c
mistpvd <mistpvd@gmail.com>
2025-11-10 16:55:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import React from "react"; 
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   build: {
-    sourcemap: false
-  }
-})
+    sourcemap: false,
+  },
+});
+
 
